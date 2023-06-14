afde4a326da2433a8c35223006cdb39389c73bb9
danchaogu <danchao.gu@emqx.io>
2023-06-14 17:07:04 +0800

    migrate from vuepress to vitepress

--- preview.sh
+++ preview.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 ## this script is to run a docker container to
-## 1. render the markdown files for vuepress
-## 2. serve the rendered HTML pages in a vuepress site
+## 1. render the markdown files for vitepress
+## 2. serve the rendered HTML pages in a vitepress site
 ##
-## It takes one argument as the listner port number the
+## It takes one argument as the listener port number the
 ## port number which defaults to 8080
 
 PORT="${1:-8080}"
@@ -15,10 +15,9 @@ THIS_DIR="$(cd "$(dirname "$(readlink "$0" || echo "$0")")"; pwd -P)"
 docker rm cloud-doc-preview || true
 
 docker run -p ${PORT}:8080 -it --name cloud-doc-preview \
-    -v "$THIS_DIR"/directory.json:/app/docs/.vuepress/config/directory.json \
+    -v "$THIS_DIR"/directory.json:/app/docs/.vitepress/config/directory.json \
     -v "$THIS_DIR"/en_US:/app/docs/en/cloud/latest \
     -v "$THIS_DIR"/zh_CN:/app/docs/zh/cloud/latest \
     -e DOCS_TYPE=cloud \
     -e VERSION=latest \
-    --platform linux/amd64 \
-ghcr.io/emqx/docs-emqx-com-frontend:latest
+ghcr.io/emqx/docs-emqx-com-next:latest
